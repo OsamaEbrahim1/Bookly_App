2aaf78f35da2171057f8893d5f5fb227081db344
osama <osamaluufy@gmail.com>
2024-09-15 15:52:08 +0300

    first commit

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errMessage;
+
+  const Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+
+      case DioExceptionType.badResponse:
+        if (dioError.response != null) {
+          return ServerFailure.fromResponse(
+              dioError.response!.statusCode, dioError.response!.data);
+        } else {
+          return ServerFailure('Received an invalid response from the server');
+        }
+
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to ApiServer was canceled');
+
+      case DioExceptionType.unknown:
+        if (dioError.message!.contains('SocketException')) {
+          return ServerFailure('No Internet Connection');
+        }
+        return ServerFailure('Unexpected Error, Please try again!');
+
+      default:
+        return ServerFailure('Oops! There was an error, please try again.');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int? statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your request not found, Please try later!');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal Server error, Please try later');
+    } else {
+      return ServerFailure('Opps There was an Error, Please try again');
+    }
+  }
+}
